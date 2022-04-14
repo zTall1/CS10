861d9fd4c1e20b5c6e20aa5844d003a851d21bad
1100037074 <1100037074@L815WD220018.edu.cbe.ab.ca>
2022-04-14 14:59:14 -0600

    2:59 PM

---
+++ Credit 3/src/Assignments/Assignment4.java
@@ -0,0 +1,34 @@
+package Assignments;
+
+import java.util.Scanner;
+
+/*
+
+Program: ${Assignment4}.java          Date: ${April 14, 2022}
+
+
+Author: Isaac Daodu
+School: CHHS
+Course: Computer Science 10
+ 
+*/
+public class Assignment4 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Diameter of the pizza in inches:");
+		int red = input.nextInt();
+		
+		double darkRed = ((0.05*red*red)+0.75+1);
+		System.out.print("The cost of your pizza is: $" + darkRed);
+		
+
+	}
+
+}
+/* Screen Dump
+Diameter of the pizza in inches:
+ */
