9d6b255409dc9bfa976b8ef0116775da2f82d264
1100037074 <1100037074@L815WD220018.edu.cbe.ab.ca>
2022-05-02 13:59:19 -0600

    1:59 PM

---
+++ Credit 5/src/Assignments/EvenOrOdd.java
@@ -0,0 +1,40 @@
+package Assignments;
+/*
+
+Program: ${EvenOrOdd}.java          Date: ${May 2, 2022}
+
+
+Author: Isaac Daodu
+School: CHHS
+Course: Computer Science 10
+ 
+*/
+import java.util.Scanner;
+
+public class EvenOrOdd 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in); //allows the program to scan and save user input
+		System.out.println("Enter an integer:"); //prints Enter an integer:
+		int num1 = input.nextInt(); //saves a variable for the user input
+		String One = "Even"; //creates a string with the value Even
+		String Two = "Odd"; //creates a string with the value Odd
+		if (num1%2 == 0)  //creates a condition if (num1%2 == 0)
+		{ 
+			System.out.println(One); //prints One if the condition is correct
+		}
+		else
+		{
+			System.out.println(Two); //prints Two if the condition is false
+		}
+		
+	}
+}
+/* Screen Dump
+Enter an integer:
+999
+Odd
+
+	 */
