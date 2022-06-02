b32caf0624b86556bdb789f729d0fbaada62a5f6
1100037074 <1100037074@L815WD220020.edu.cbe.ab.ca>
2022-06-02 15:17:18 -0600

    3:17pm

---
+++ Credit 5/src/JavaProject/JavaProject.java
@@ -0,0 +1,15 @@
+package JavaProject;
+
+import java.util.Scanner;
+
+public class JavaProject 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner userinput = new Scanner(System.in); //allows program to scan and saves user input
+		
+
+	}
+
+}
