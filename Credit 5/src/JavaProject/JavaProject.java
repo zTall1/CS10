15f244614ffbe973c5c0ca19b8e254fffffca51a
1100037074 <1100037074@L815WD220020.edu.cbe.ab.ca>
2022-06-07 15:30:19 -0600

    3:30PM

--- Credit 5/src/JavaProject/JavaProject.java
+++ Credit 5/src/JavaProject/JavaProject.java
@@ -8,6 +8,10 @@ public class JavaProject
 	public static void main(String[] args) 
 	{
 		Scanner userinput = new Scanner(System.in); //allows program to scan and saves user input
+		String a = "Aboriginal studies 10_5 cr";
+		String b = "CALM";
+		System.out.println("Please enter -1 to view the course catalog or enter desired courses" );
+
 		
 
 	}
